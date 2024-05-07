921606cfdc622e720dfe23d9615d40652fea7a89
sumitnair26 <sumit.nair26@gmail.com>
2024-05-07 20:49:38 +0530

    added ws connection code

---
+++ backend1/src/GameManager.ts
@@ -0,0 +1,19 @@
+
+export class GameManager {
+    private games: Game[];
+    constructor() {
+        this.games = [];
+    }
+
+    addUser(socket: WebSocket){
+
+    }
+
+    removeUser(socket: WebSocket){
+
+    }
+
+    private handleMessage() {
+
+    }
+}
