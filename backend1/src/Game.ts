68f465152f81cba177b98aa7c111de65c7c4245f
sumitnair26 <sumit.nair26@gmail.com>
2024-05-08 21:45:43 +0530

    added Make Move logic using chess.js library to make moves

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,16 +1,54 @@
+import { Chess } from "chess.js";
 import { WebSocket } from "ws";
+import { GAME_OVER, MOVE } from "./Messages";
 export class Game {
-    private player1: WebSocket;
-    private player2: WebSocket;
-    private board: string;
-    private moves: string[];
+    public player1: WebSocket;
+    public player2: WebSocket;
+    public board: Chess;
     private startTime: Date;
 
     constructor(player1: WebSocket, player2: WebSocket){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.moves = [];
+        this.board = new Chess();
         this.startTime = new Date();
+    }
+
+    makeMove(socket: WebSocket, move: {
+        from: string,
+        to:string
+    }) {
+
+      /*  validation
+        is it the users move
+        is the move valid 
+        Validate the type of move
+        */
+        try {
+            this.board.move(move)
+        } catch (error) {
+            return;
+        }
+
+        if(this.board.isGameOver()){
+            this.player1.emit(JSON.stringify({
+                type: GAME_OVER,
+                payload:{
+                    winner: this.board.turn() ==="w" ? "black" : "white" 
+                }
+            }))
+        }
+
+        if (this.board.moves.length % 2 ===0) {
+            this.player2.emit(JSON.stringify({
+                type:MOVE,
+                payload:move
+            }))
+        }else{
+            this.player1.emit(JSON.stringify({
+                type:MOVE,
+                payload:move
+            }))
+        }        
     }
 }
--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,12 +1,15 @@
-import { INIT_GAME } from "./Messages";
+import { Game } from "./Game";
+import { INIT_GAME, MOVE } from "./Messages";
 
 export class GameManager {
     private games: Game[];
-    private pendingUser: WebSocket;
+    private pendingUser: WebSocket | null;
     private users: WebSocket[];
 
     constructor() {
         this.games = [];
+        this.pendingUser = null;
+        this.users = [];
     }
 
     addUser(socket: WebSocket){
@@ -22,11 +25,21 @@ export class GameManager {
         socket.on("message", (data)=> {
             const message = JSON.parse(data.toString());
 
-            if(message.type == INIT_GAME){
+            if(message.type === INIT_GAME){
                 if(this.pendingUser) {
                     //start a game
+                    const game = new Game(this.pendingUser, socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
                 }else {
                     this.pendingUser = socket;
+                }
+            }
+
+            if (message.type === MOVE) {
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket );
+                if (game) {
+                    game.makeMove(socket, message.move);
                 }
             }
         })
