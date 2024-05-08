6129e2e77931f217112e5f74e7d745d0164e3e4c
sumitnair26 <sumit.nair26@gmail.com>
2024-05-08 14:38:17 +0530

    Handele Message

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,19 +1,34 @@
+import { INIT_GAME } from "./Messages";
 
 export class GameManager {
     private games: Game[];
+    private pendingUser: WebSocket;
+    private users: WebSocket[];
+
     constructor() {
         this.games = [];
     }
 
     addUser(socket: WebSocket){
-
+        this.users.push(socket);
     }
 
     removeUser(socket: WebSocket){
-
+        this.users = this.users.filter(user => user !== socket);
+        //Stop the Game here as the user has left
     }
 
-    private handleMessage() {
+    private handleMessage(socket: WebSocket) {
+        socket.on("message", (data)=> {
+            const message = JSON.parse(data.toString());
 
+            if(message.type == INIT_GAME){
+                if(this.pendingUser) {
+                    //start a game
+                }else {
+                    this.pendingUser = socket;
+                }
+            }
+        })
     }
 }
