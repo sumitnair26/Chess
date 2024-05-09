828cf687588e0744670e5cea98b5c6bb89b702a3
sumitnair26 <sumit.nair26@gmail.com>
2024-05-09 20:39:49 +0530

    Play Game page structure

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,5 +1,14 @@
 export const Game = () => {
-    return <div>
-        Game screen
+    return <div className="justify-center flex">
+        <div className="pt-8 max-w-screen-lg">
+            <div className="grid grid-col-1 gap-4 md:grid-cols-2">
+                <div>
+                    <ChessBoard />
+                </div>
+                <div>
+                    <button>Play</button>
+                </div>
+            </div>
+        </div>
     </div>
 }
