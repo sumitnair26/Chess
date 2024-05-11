35ec5aedc1d0f42de7ac7afd8e1bea3e86546f22
sumitnair26 <sumit.nair26@gmail.com>
2024-05-11 21:41:25 +0530

    added Hook useSocket

---
+++ frontend/src/hooks/useSocket.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const WS_URL = "";
+
+export const useSockets = () => {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    
+    useEffect(()=>{
+        const ws = new WebSocket(WS_URL);
+        ws.onopen = () => {
+            setSocket(ws);
+        }
+
+        ws.onclose = () => {
+            setSocket(null);
+        }
+
+        return () => {
+            ws.close();
+        }
+    },[])
+
+    return socket;
+
+}
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,7 +1,13 @@
 import { Button } from "../components/Button"
 import { ChessBoard } from "../components/ChessBoard"
+import { useSockets } from "../hooks/useSocket"
+export const INIT_GAME = "init_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over"
 
 export const Game = () => {
+    const socket = useSockets();
+    if(!socket) return <div>Connecting ...........</div>
     return <div className="justify-center flex">
         <div className="pt-8 max-w-screen-lg w-full">
             <div className="grid grid-cols-6 gap-4 w-full">
@@ -9,7 +15,11 @@ export const Game = () => {
                     <ChessBoard />
                 </div>
                 <div className="col-span-2 bg-green-200 w-full">
-                    <Button> Play</Button>
+                    <Button onClick={()=>{
+                        socket.send(JSON.stringify({
+                            type:INIT_GAME,
+                        }))
+                    }}> Play</Button>
                 </div>
             </div>
         </div>
